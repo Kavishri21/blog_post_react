3bf4a181e710962cd8c2bd6524b5366418e88b91
Kavishri21 <dr.kavi21k@gmail.com>
2026-03-24 21:54:25 +0530

    integrated redux easy-peasy

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; 
+import ReactDOM from 'react-dom/client'; 
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { StoreProvider } from 'easy-peasy';
+import store from './store';
 
+// 1. Create the root first
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// 2. Render to that root
 root.render(
   <React.StrictMode>
-    <Router>
-      <App /> {/* index.js ONLY wraps App in a Router */}
-    </Router>
+    <StoreProvider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </StoreProvider>
   </React.StrictMode>
 );
-

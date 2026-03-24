de5967c1b85bd329350143b7fa77e7dc0800366a
Kavishri21 <dr.kavi21k@gmail.com>
2026-03-24 20:46:02 +0530

    added hooks using axios

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import api from './api/posts';
+import useWindowSize from './hooks/useWindowSize';
+import useAxiosFetch from './hooks/useAxiosFetch';
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -20,26 +22,15 @@ function App() {
   const [postBody, setPostBody] = useState('');
   const [editTitle, setEditTitle] = useState('');
   const [editBody, setEditBody] = useState('');
+  const { width } = useWindowSize();
+
+const { data, fetchError, isLoading } = useAxiosFetch('http://localhost:3500/posts');
   
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await api.get('/posts');
-        setPosts(response.data);
-      } catch (err) {
-        if (err.response) {
-          console.log(err.response.data);
-          console.log(err.response.status);
-          console.log(err.response.headers);
-        } else {
-          console.log(`Error: ${err.message}`);
-        }
-      }
-    }
-    fetchPosts();
-  }, [])
+ useEffect(() => {
+    setPosts(data);
+  }, [data])
 
   useEffect(() => {
     const filteredResults = posts.filter((post) =>
@@ -93,11 +84,15 @@ function App() {
 
   return (
     <div className="App">
-      <Header title="React JS Blog" />
+      <Header title="React JS Blog" width={width} />
       <Nav search={search} setSearch={setSearch} />
       
       <Routes>
-        <Route path="/" element={<Home posts={searchResults} />} />
+        <Route path="/" element={<Home 
+            posts={searchResults}
+            fetchError={fetchError}
+            isLoading={isLoading} 
+            />} />
 
         <Route path="/post" element={
           <NewPost
